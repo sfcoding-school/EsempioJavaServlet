1670f1c10d81455e98ff4a791d4ba02c9d79a338
lorenzo <lupacche@gmail.com>
2015-08-18 16:21:49 +0200

    init3

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 make
+#copio class files
+sudo cp -r classes/ /var/lib/tomcat7/webapps/prova1/
 
+#copio DD
+sudo cp web/web.xml /var/lib/tomcat7/webapps/prova1/WEB_INF/web.xml
 
+sudo service tomcat7 restart
