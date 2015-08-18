c53577255e388a1de0382e863ada5d5d8b65e8ad
lorenzo <lupacche@gmail.com>
2015-08-18 16:00:57 +0200

    init2

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 all:
 	javac -d classes src/com/example/model/Model.java
-	javac -classpath /usr/share/tomcat7/lib/servlet-api.jar -d classes src/com/example/web/View.java
+	javac -sourcepath src/ -classpath /usr/share/tomcat7/lib/servlet-api.jar -d classes src/com/example/web/View.java
--- src/com/example/web/View.java
+++ src/com/example/web/View.java
@@ -7,11 +7,13 @@ import java.util.*;
 
 public class View extends HttpServlet {
 	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
-		String c = request.getParameter("color");
+		//String c = request.getParameter("color");
 		Model modello = new Model();
 		String result = modello.saluta();
+		
 		response.setContentType("text/html");
+		
 		PrintWriter out = response.getWriter();
-		out.println("Beer Selection Advice<br>");
+		out.println(result);
 	}
 }
